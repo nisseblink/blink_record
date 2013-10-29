0eb06e9dfca9702c1146c31e0c618c9624bdd6a7
Nisse <nisseblink@gmail.com>
2013-10-29 19:49:49 +0100

    First working version.

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
 blink_record
 ============
 
-A wrapper script to Team Fortress 2 too ease recording using linux in the absence of prec.
+A wrapper script to Team Fortress 2 (tf2) too ease recording using linux in the absence of prec.
+
+FNAQ
+===========
+
+OMG WFT .EXE VAC BANNED!? etc
+------------
+No, this setup does not inject itself in any way or do anything but:
+* Locate demo files in tf directory.
+* Parse headers of demo files.
+* Rename the files.
+* Start Team Fortress 2.
+* And we are done.
+
+How do I install it?
+----------------
+* Make backup of your existing configs. You should always have a backup...
+* Download the archive, extract and run the `install.sh` (In a terminal preferably)
+* Open blink_record that you should be able to find through your favorite application launcher (it will launch tf2).
+* Open console and type `bind KEY blink_record` e.g. `bind f6 blink_record`.
+* If you have unbindall you need to edit them and add `bind KEY blink_record` at the appropriate location(s).
+
+How do I use it?
+================
+* Use the blink_recorder to launch tf2.
+* Use your binded key to cycle start/stop recording.
+* Next time you launch tf2 all your demo files will be renamed.
+
+Requirements
+============
+The automatic script is built for Ubuntu/Debian/Linux Mint etc and it needs apt-get to install the *ruby* environment.
+If you have some other distribution, install *ruby* before and you should be fine.
+
+Automatic installation
+----------------------
+* gksudo (used for installing ruby and copy binary and application launcher)
+* Team Fortress 2
+
+Application
+---------------------
+* Ruby
+* Team Fortress 2
+
+How does it work
+================
+There is two components to this setup, tf2 script and a wrapper.
+
+Team Fortress 2 script
+----------------------
+
+This add a couple of aliases for a cycle of 20 recordings.
+The installer also adds execution of the config in autoexec if it exists or adds this file.
+
+Wrapper
+-------
+As stated in **OMG WFT .EXE VAC BANNED!? etc**
+
+Source code
+===========
+I encourage you to browse through the source code and look for yourself what is happening.
+There are < 200 lines of code so it should not be a problem.
+
+Feel free to contribute to the project by sending me changes/bugfixes or simply bugreports :)
+
+Future possible features
+========================
+
+So there might be a way to achive a similar feature as prec's bookmark.
+Technically this could be achieved by using a dekstop global keybinding that
+calls a script that logs the time to a file and then handle it
+when renaming the demo files. Let me know what you think about this.
---
+++ install.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#Abort if some command fail.
+set -e
+DESKTOPSHORTCUT="$(xdg-user-dir DESKTOP)/blink_record.desktop"
+SYSTEMSHORTCUT='/usr/share/applications/blink_record.desktop'
+BINARYPATH='/usr/local/bin/blink_record'
+
+function ulog {
+	#echo "$1" >> uninstall.log
+}
+
+function ilog {
+	#echo "$1" >> install.log
+}
+
+#Get the tf2 dir as stated in the appmanifest.
+function get_tf2_dir {
+	tf2_dir=`cat $HOME/.steam/steam/SteamApps/appmanifest_440.acf | sed -n -e 's/.*\"appinstalldir\".*\"\(.\+\)\"/\1/p'`
+	if [ -z "$tf2_dir" ]; then
+		echo 'Could not find tf2 installation directory'
+		echo 'Aborting...'
+		exit 1
+	fi
+	echo $tf2_dir
+}
+
+#Uninstall blink_record
+function uninstall {
+	ulog "====UNINSTALL BEGIN===="
+	rm $DESKTOPSHORTCUT || true
+	gksudo -- rm $SYSTEMSHORTCUT || true
+	gksudo -- rm $BINARYPATH || true
+	tf2_dir=$(get_tf2_dir)
+	rm -r "$tf2_dir/tf/custom/blink_record" || true
+	find "$tf2_dir/tf/custom" -name autoexec.cfg |
+	while read -r filename; do
+		ulog "Removing ecex blink_record from file: $filename"
+		sed -i '/^exec blink_record \/\/88b9a67a-815c-4e1a-b4b2-ffbac1fddff2$/d' "$filename"
+	done
+	echo 'Uninstall complete'
+	ulog "====UNINSTALL DONE===="
+}
+
+#Install blink_record
+function install {
+	ilog "====INSTALLATION BEGIN===="
+	tf2_dir=$(get_tf2_dir)
+	cp -r tf2_cfg/blink_record "$tf2_dir/tf/custom/"
+	#Check if unbindall is used and infor the user.
+	if grep -r -q '^unbindall' "$tf2_dir/tf/custom" ; then
+		echo 'Found unbindall. You need to bind blink_record in your config in the appropriate place(s).'
+		ilog "Found unbindall in configs"
+	else
+		echo 'Did not find unbindall in custom configs. It should be enough to bind blink_record in console.'
+		ilog "Did not find unbindall in configs"
+	fi
+	#Add config to autoexec
+	found_autoexec=false
+	#Could probably use something other than find.
+	while read -r filename; do
+		ilog "Found a autoexec @ $filename"
+		found_autoexec=true
+		if grep -q '^exec blink_record$' "$filename" ; then
+			echo 'Config already added to autoexec.'
+			ilog "Autoexec already contained exec blink_record"
+		else
+			ilog "Adding exec blink_record to $filename"
+			echo 'exec blink_record //88b9a67a-815c-4e1a-b4b2-ffbac1fddff2' >> "$filename"
+		fi
+	done < <(find "$tf2_dir/tf/custom" -name autoexec.cfg)
+	if [ $found_autoexec == false ]; then
+		ilog "Did not find autoexec file. Adding one."
+		echo 'exec blink_record //88b9a67a-815c-4e1a-b4b2-ffbac1fddff2' > "$tf2_dir/tf/custom/blink_record/cfg/autoexec.cfg"
+		echo 'Did not find autoexec so added one to path'
+		echo "$tf2_dir/tf/custom/blink_record/cfg/autoexec.cfg"
+		echo 'If you have one. Feel free to add blink_record to it and remove this file.'
+	fi
+	#Install ruby if it's mising
+	command -v ruby >/dev/null 2>&1 || { gksudo -- apt-get install --yes ruby; }
+	#Add menu items/shortcuts
+	gksudo -- cp bin/blink_record $BINARYPATH
+	cp data/blink_record.desktop $DESKTOPSHORTCUT
+	gksudo -- "cp data/blink_record.desktop $SYSTEMSHORTCUT"
+	echo 'Installation complete.'
+	ilog "====INSTALLATION DONE===="
+}
+
+function usage {
+	echo 'Usage: install [-u]'
+	exit 2
+}
+
+#Some minor usage check
+if [ $# -gt 1 ]; then
+	usage
+elif [ $# -eq 1 ]; then
+	if [ $1 == '-u' ]; then
+		uninstall
+	else
+		usage
+	fi
+else
+	install
+fi
